80f7eaddb5c1d971f63c6d1f45678d7892319164
luanhoffmann <61809243+luanlipperthoffmann@users.noreply.github.com>
2021-08-25 22:44:05 -0300

    TDE_Programação_Web_Aula05

--- 2021-02/4° - Programação Web/docker-lamp-pw-master/www/views/client/registerView.php
+++ 2021-02/4° - Programação Web/docker-lamp-pw-master/www/views/client/registerView.php
@@ -34,4 +34,4 @@
         </tr>
     </table>
 </div>
-<button class="btn btn-primary"  onclick="printDiv()">Imprimir cadastro</button>
+<button class="btn btn-primary"  onclick="printDiv()">Imprimir Cadastro</button>
